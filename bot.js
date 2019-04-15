96b7b0073839d8888ace3c89dd70bcfeb931ca8b
aalixp123 <49370127+aalixp123@users.noreply.github.com>
2019-04-15 17:17:06 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -217,7 +217,7 @@ fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
     if (err) console.error(err);
   });
   credits[message.author.id] = {
-      credits: m + 0.5,
+      credits: m + 1000,
   }
   
     if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
@@ -226,7 +226,7 @@ message.channel.send(`**${message.author.username}, your :credit_card: balance i
 });
 
 client.on('message', async message => {
-    let amount = 10000000000;
+    let amount = 250;
     if(message.content.startsWith(prefix + "daily")) {
     if(message.author.bot) return;
     if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
